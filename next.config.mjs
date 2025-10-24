a50104baf97fe1464ba927a6e9f8bb6ddffbc3a1
Daniel Tome Fernandez <danieltomefer@gmail.com>
2025-10-24 13:13:59 +0200

    feat: add webpack configuration to suppress warnings for optional dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,18 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  webpack: (config, { isServer }) => {
+    // Suppress warnings for optional dependencies that aren't needed in browser
+    config.ignoreWarnings = (config.ignoreWarnings || []).concat([
+      // Suppress MetaMask SDK warnings about React Native dependencies
+      /Can't resolve '@react-native-async-storage\/async-storage'/,
+      /Can't resolve 'react-native'/,
+      /Can't resolve 'expo-modules-core'/,
+      /indexedDB is not defined/,
+    ]);
+
+    return config;
+  },
 }
 
 export default nextConfig
