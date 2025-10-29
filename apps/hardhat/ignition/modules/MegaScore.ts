b8f2544f3f1f39f75405ffd376f794328af76231
Daniel Tome Fernandez <danieltomefer@gmail.com>
2025-10-29 09:16:12 +0100

    refactor: update MegaScore module to include minting and updating prices, recipient address, and payment token

--- apps/hardhat/ignition/modules/MegaScore.ts
+++ apps/hardhat/ignition/modules/MegaScore.ts
@@ -1,7 +1,22 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 export default buildModule("MegaScoreModule", (m) => {
-  const megaScore = m.contract("MegaScore");
+  const mintPrice = BigInt(0);
+  const updatePrice = BigInt(0);
+  const recipientAddr =
+    "0xaa4C60b784E2b3E485035399bF1b1aBDeD66A60f" as `0x${string}`;
+  const paymentToken = m.contract("MockERC20", [
+    "MegaScore Token",
+    "MSCORE",
+    1000000000n * 10n ** 18n,
+  ]);
 
-  return { megaScore };
+  const megaScore = m.contract("MegaScore", [
+    mintPrice,
+    updatePrice,
+    recipientAddr,
+    paymentToken,
+  ]);
+
+  return { megaScore, paymentToken };
 });
